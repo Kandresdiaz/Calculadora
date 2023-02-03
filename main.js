7f30970b1d8d4fc91949737453843c5b12d1fdeb
ferney <ferney.rojas027@gmail.com>
2023-02-03 14:57:13 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let uno = document.getElementById('uno')
+let suma = document.getElementById('suma')
+let pantalla= document.getElementById('pantalla')
+suma.addEventListener('click',()=>{
+pantalla.innerHTML()
+
+})
