051625e594529c6de0191a3615d6bbf437ccd788
ferney <ferney.rojas027@gmail.com>
2023-02-03 17:24:28 -0500

    agregando text

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
-let uno = document.getElementById('uno')
-let suma = document.getElementById('suma')
-let pantalla= document.getElementById('pantalla')
-suma.addEventListener('click',()=>{
-pantalla.innerHTML()
+let pantalla= document.getElementById("pantalla")
+let operador=document.querySelectorAll('.operador')
+let numero =document.querySelectorAll('.boton')
 
+//aca pinto los numeros
+function capturarNumero(x){
+    pantalla.innerHTML+=x
+}
+function capturarOperador(x){
+    pantalla.innerHTML += operador[x].value 
+} 
+
+numero.forEach((x,i)=>{
+    if(i===9){
+        x.addEventListener('click',()=>capturarNumero(0))
+    }else{
+        x.addEventListener('click',()=>capturarNumero(i+1))
+    }
 })
+
+operador.forEach((x,i)=>{
+    x.addEventListener('click',()=>capturarOperador(i))
+})
+//operador[0]=suma
+//operador[1]=resta
+//operador[2]=multi
+//operador[3]=divi
+
+
+
+
+/* function operador(x){
+switch(x){
+    case '+':
+
+    break;
+}
+} */
